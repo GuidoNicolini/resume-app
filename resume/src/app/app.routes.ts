7f178c1c885d1be4064c4849a006adf9a92b7e64
GuidoNicolini <guidonicolini123@hotmail.com>
2024-07-19 22:03:42 -0300

    add mains components
    -add main component
    -add experience component
    -add education component
    -add contacts component

--- resume/src/app/app.routes.ts
+++ resume/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:"", component: MainPageComponent },
+    { path:"experience", component: ExperienceComponent },
+    { path:"education", component: EducationComponent },
+    { path:"contact", component: ContactComponent },
+];
